329cec846ae7e9daf3eda6d97a29c4dd6cf50498
Max <maxrossfl@yahoo.com>
2023-02-07 16:18:23 -0500

    auth completed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,18 @@
 import { SessionProvider } from "next-auth/react"
 import type { AppProps } from "next/app"
 import type { Session } from "next-auth"
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import React from 'react';
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps: { ...pageProps },
 }: AppProps<{ session: Session }>) {
   return (
-    <SessionProvider session={session}>
+    <UserProvider>
       <Component {...pageProps} />
-    </SessionProvider>
-  )
+    </UserProvider>
+  );
 }
