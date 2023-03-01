e38a35198b3c06c51ad76abd57935d226f04c019
Max <maxrossfl@yahoo.com>
2023-03-01 18:03:27 -0500

    frontend updates for functionality. doesnt show games that have no image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,17 @@
 // pages/_app.js
-import '@/src/styles/globals.css'
-import React, { useEffect } from 'react';
-import { UserProvider } from '@auth0/nextjs-auth0/client';
-import type { AppProps } from 'next/app';
-import { AnimatePresence } from "framer-motion"
-
+import "@/src/styles/globals.css";
+import React, { useEffect } from "react";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import type { AppProps } from "next/app";
+import { AnimatePresence } from "framer-motion";
 
 export default function App({ Component, pageProps }: AppProps) {
-
   return (
+    //  <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
     <UserProvider>
-       <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
       <Component {...pageProps} />
-      </AnimatePresence>
     </UserProvider>
+    // </AnimatePresence>
   );
 }
+
