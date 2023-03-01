8a7c3c5abdb382e8dc6171a0c6ffe1a89747994c
Max <maxrossfl@yahoo.com>
2023-02-28 21:09:48 -0500

    beautiful

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -2,6 +2,8 @@ import Navbar from "../components/navbar";
 import { Input } from "@/src/components/ui/input";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import axios from "axios";
+import Loader from "../components/loader";
+import Image from "next/image";
 
 interface Game {
   id: string;
@@ -22,53 +24,58 @@ export const getGames = async (query: string) => {
   }
 };
 
-const DEFAULT_DATA: Game[] = [];
-
-const DEBOUNCE_DELAY = 500;
-
 export default function Games() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState<Game[]>(DEFAULT_DATA);
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState<Game[]>([]);
   const [query, setQuery] = useState("");
-  // const [debounceTime, setDebounceTime] = useState(Date.now());
 
   useEffect(() => {
     (async () => {
       if (query === "") return;
-      // if (Date.now() - debounceTime < DEBOUNCE_DELAY) return;
-
       setIsLoading(true);
       const games = await getGames(query);
       setData(games);
       setIsLoading(false);
       console.log(data);
-
-      // setDebounceTime(Date.now());
     })();
-  }, [data, query]);
+  }, [query]);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
   };
 
   return (
-    <div>
+    <div className="mb-10">
       <Navbar />
-      <div className="mx-auto mt-10 w-4/5">
-        <Input
+      <div className="mx-auto mt-10 w-4/5 max-w-6xl">
+        <input
+          className="block w-full rounded-md border border-gray-200 py-3 px-4 text-sm focus:border-blue-500 focus:ring-blue-500 dark:border-gray-700 dark:bg-slate-900 dark:text-gray-400"
           type="text"
           placeholder="Search games"
           onChange={handleInputChange}
         />
         {isLoading ? (
-          <p>Loading...</p>
+          <Loader />
         ) : (
-          data.map((game) => (
-            <div key={game.id}>
-              <h2>{game.name}</h2>
-              <img src={game.background_image} alt={game.slug} />
-            </div>
-          ))
+          <div className="mt-5 grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {data.map((game) => (
+              <a
+                className="flex flex-col rounded-xl border bg-white shadow-sm transition hover:shadow-lg dark:border-gray-700 dark:bg-gray-800 dark:shadow-slate-700/[.7]"
+                href="#"
+              >
+                <img
+                  className="h-[200px] w-full overflow-clip rounded-t-xl"
+                  src={game.background_image}
+                  alt={game.slug}
+                />
+                <div className="p-2 md:p-3">
+                  <h3 className="truncate text-lg font-bold text-gray-800 dark:text-white">
+                    {game.name}
+                  </h3>
+                </div>
+              </a>
+            ))}
+          </div>
         )}
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,10 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [require("daisyui"), require ("tailwindcss-animate")],
-// }
-
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class", '[data-theme="dark"]'],
-  content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",'node_modules/preline/dist/*.js',
+  ],
   theme: {
-    extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
+    extend: {},
   },
-  plugins: [require("daisyui"), require ("tailwindcss-animate")]
+  plugins: [require('preline/plugin')],
 }
